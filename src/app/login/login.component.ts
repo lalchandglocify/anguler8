24e0e5e11e831fb8e7401f868d590dbca7c43479
lalchand <lalchand.glocify@gmail.com>
2020-01-15 15:15:26 +0530

    test comment.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
